4f1d8a6fdd1c390a2b000ffff018317eab1d47b1
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2022-02-02 22:56:02 +0530

    Fetching the posts

---
+++ src/actions/posts.js
@@ -0,0 +1,24 @@
+import {UPDATE_POSTS} from './actionTypes.js'
+
+export function fetchPosts(){
+    return (dispatch) => {
+        const url = 'http://codeial.com:8000/api/v2/posts?page=1&limit=5'
+        fetch(url)
+        .then(respose => {
+            console.log("SDas");
+            return respose.json();
+        }).then((data) => {
+            console.log(data);
+            dispatch(updatePosts(data.data.posts));
+        })
+    
+    }
+}
+
+
+export function updatePosts(posts){
+    return {
+        type:UPDATE_POSTS,
+        posts
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 
 
 
-function App() {
-  return (
-    <div className="App">
-        <h1>APP</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import {fetchPosts} from '../actions/posts'
+class App extends Component {
+  componentDidMount() {
+    console.log("dsfsdf")
+    this.props.dispatch(fetchPosts());
+  }
+  render() {
+    return (
+      <div>
+        APP
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts
+  }
+
+}
+
+
+
+export default connect(mapStateToProps)(App);
