8b13683a295ae6fb72c28f5342b4e9ee52c8d10a
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2022-02-03 21:34:40 +0530

    Adding PropTypes

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 class PostsList extends Component {
   render() {
     const { posts } = this.props;
@@ -60,4 +60,7 @@ class PostsList extends Component {
   }
 }
 
+PostsList.propTypes = {
+  posts: PropTypes.array.isRequired,
+}
 export default PostsList;
