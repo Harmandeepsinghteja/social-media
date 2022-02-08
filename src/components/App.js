e6a70c39c96b48520362187b1159bad12de9a696
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2022-02-08 22:02:12 +0530

    Bug Fix

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
-import { PostsList } from './';
+import  PostsList   from './PostsList';
 
 class App extends React.Component {
   componentDidMount() {
