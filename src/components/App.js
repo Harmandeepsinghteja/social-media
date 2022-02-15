7d2c4ff4ee0d54a1bf3f3afd68e5f50f7f17516f
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2022-02-15 21:41:31 +0530

    Creating Private Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Link , Route , Routes} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import * as jwtDecode from 'jwt-decode';
 import { fetchPosts } from '../actions/posts';
-import  PostsList   from './PostsList';
+import PostsList from './PostsList';
 // import {Home,Navbar} from '.';
-import  Home  from './Home';
-import  Navbar  from './Navbar';
+import Home from './Home';
+import Navbar from './Navbar';
 import Page404 from './Page404';
-import Login  from './Login';
-import  Signup  from './Signup';
+import Login from './Login';
+import Signup from './Signup';
 import { authenticateUser } from '../actions/auth';
 // const Home = () => {
 // return <div>Home</div>
 // }
 
-
-
-
-
+const Settings = () => <div>Settings</div>;
+// const PrivateRoute = (privateRouteProps) => {
+//   const { isLoggedin, path, component: Component } = privateRouteProps;
+ 
+ 
+//   return (
+//     <Route
+//       path={path}
+//       element={(props) => {
+//         return isLoggedin ? <Component {...props} /> : <Navigate to="/login" />;
+//       }}
+//     />
+//   );
+// };
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
     const token = localStorage.getItem('token');
 
-    if(token) {
+    if (token) {
       const user = jwtDecode(token);
       console.log(user);
-      this.props.dispatch(authenticateUser({
-        email: user.email,
-        _id: user._id,
-        name: user.name
-      }))
+      this.props.dispatch(
+        authenticateUser({
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+        })
+      );
     }
   }
 
   render() {
-    const { posts } = this.props;
+    const { posts, auth } = this.props;
     return (
       <Router>
         <div>
@@ -48,7 +66,13 @@ class App extends React.Component {
 
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route  element={<Page404 />} />
+            <Route
+              path="/settings"
+              element={<Settings />}
+              isLoggedin={auth.isLoggedin}
+            />
+
+            <Route element={<Page404 />} />
           </Routes>
         </div>
       </Router>
@@ -59,6 +83,7 @@ class App extends React.Component {
 function mapStateToProps(state) {
   return {
     posts: state.posts,
+    auth: state.auth,
   };
 }
 
