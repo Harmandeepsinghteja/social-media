b72fe2a7ff0a4538a4186d8046669c110fb1d89a
HARMAN TEJA <harmandeepsinghteja@gmail.com>
2022-02-28 21:07:53 +0530

    Creating Post UI

---
+++ src/components/CreatePost.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+    };
+  }
+  handleChange = (e) => {
+      this.setState({
+          content: e.target.value,
+      })
+  };
+  handleOnClick = () => {
+    //   dispatch an action
+  };
+  render() {
+    return (
+      <div className="create-post">
+        <textarea
+          className="add-post"
+          value={this.state.content}
+          onChange={this.handleChange}
+        />
+        <div>
+          <button id="add-post-btn" onClick={this.handleOnClick}>
+            Add Post
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreatePost;
